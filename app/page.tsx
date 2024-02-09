cac3b863347a9846e17cd54608b5c7b51f55499b
harkanni <harkanzyempire001@gmail.com>
2024-02-09 21:56:56 +0100

    Evaluation component

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                      </ul>
                   </div>
                   <div className={`${styles.hamburger}`}>
-                     <GiHamburgerMenu size={35} color="white" className={`cursor-pointer`}/>
+                     <GiHamburgerMenu size={35} color="white" className={`cursor-pointer`} />
                   </div>
                </nav>
 
@@ -34,7 +34,7 @@ export default function Home() {
                   <h1 className={`${styles.textShadow} text-white text-4xl leading-[3rem] font-bold`}>Find out what your home is really worth</h1>
                   <div className={`p-2 pl-5 flex justify-between gap-1 bg-white rounded-lg mt-7 mb-5`}>
                      <input className={`${styles.textInput} flex-1 placeholder:hidden`} type="text" placeholder="Enter your address" />
-                     <Button variant="contained" color="primary" className={`${`button`} bg-black px-8 py-3`}>Estimate</Button>
+                     <Button variant="contained" color="primary" className={`${styles.button} bg-black`}>Estimate</Button>
                   </div>
                   <p className={`${styles.textShadow} text-white`}>Get a real-world home value estimate in less than 2 minutes.</p>
                </div>
