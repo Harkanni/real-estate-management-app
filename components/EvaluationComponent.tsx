ca560ae7a24bdda60413a732abaf55e0ca214486
harkanni <harkanzyempire001@gmail.com>
2024-02-12 21:57:10 +0100

    Evaluation Component

--- components/EvaluationComponent.tsx
+++ components/EvaluationComponent.tsx
@@ -11,14 +11,14 @@ const EvaluationComponent = () => {
          {
             Evaluations.map((item, index) => {
                return (
-                  <div className={`${index+1 >=2 ? 'flex flex-row-reverse' : 'flex'} justify-between font-open gap-4 mt-10 px-16 py-5`}>
-                     <div className={`flex flex-col gap-6 w-[50%] max-w-[30rem]`}>
+                  <div className={` ${styles.evalDiv}  ${index+1 >=2 ? 'flex flex-row-reverse' : 'flex'} justify-between font-open gap-4 mt-10 px-16 py-5`}>
+                     <div className={`${styles.evalDesc} flex flex-col gap-6 w-[50%]`}>
                         <h1 className={`font-extrabold text-3xl`}>{item.title}</h1>
                         <p className='leading-8 font-[400]'>{item.details}</p>
                         <Button variant="contained" color="primary" className={`${styles.EVbutton} bg-black w-[70px] px-[50rem]`}>Estimate</Button>
                      </div>
 
-                     <div className={`flex w-[50%]`}>
+                     <div className={`${styles.evalImg} flex w-[50%]`}>
                         <Image
                            src={item.imgUrl}
                            alt=""
