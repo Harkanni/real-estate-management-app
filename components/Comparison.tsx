6ce03d3fbf742bc8240880e123c2d1d231f700f0
harkanni <harkanzyempire001@gmail.com>
2024-02-13 07:34:50 +0100

    comparison

--- components/Comparison.tsx
+++ components/Comparison.tsx
@@ -5,21 +5,21 @@ import styles from '@/assets/style.module.css'
 
 const Comparison = () => {
   return (
-    <section>
+    <section className='p-16'>
       <ul>
          {
             Comparisons.map((item, index) => {
                return (
-                  <li key={index} className='w-[100%]'>
-                     <div className={`${styles.keypoint} inline-block w-[70%]`}>
-                        <label>{item.label}</label>
-                        <p>{item.desc}</p>
+                  <li key={index} className='w-[100%] border-b-2 py-3'>
+                     <div className={`${styles.keypoint} inline-block w-[70%] leading-8`} style={{verticalAlign: 'middle'}}>
+                        <label className='font-black'>{item.label}</label>
+                        <p className='text-[0.85rem]'>{item.desc}</p>
                      </div>
                      <div className={`${styles.comparison} inline-block w-[30%]`}>
-                        <div className='inline-block w-[50%] text-center'>
+                        <div className='inline-flex justify-center items-center w-[50%] text-center' style={{verticalAlign: 'middle'}}>
                            {item.calc}
                         </div>
-                        <div className='inline-block w-[50%] text-center'>
+                        <div className='inline-flex justify-center items-center w-[50%] text-center' style={{verticalAlign: 'middle'}}>
                            {item.agents}
                         </div>
                      </div>
