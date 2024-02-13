417ff6315eb8aa9da30e89222a0c731ba472cfca
harkanni <harkanzyempire001@gmail.com>
2024-02-13 03:13:25 +0100

    How estimates are calculated component

--- components/HomeValueEstimateInfo.tsx
+++ components/HomeValueEstimateInfo.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const HomeValueEstimateInfo = () => {
-  return (
-   <section className='p-16 my-10'>
-      <h2 className={`my-4 font-open font-extrabold text-3xl`}>How home value estimates are calculated</h2>
-   </section>
-  )
+   return (
+      <section className='p-16 my-10'>
+         <h2 className={`my-4 font-open font-extrabold text-3xl`}>How home value estimates are calculated</h2>
+         <p className='leading-8 mb-10 font-[400]'>Online home valuation tools look at millions of transactions to predict what a home is worth but they're often missing crucial data, making them inaccurate. By asking a few specific questions about your home, we can add a new layer of information to our estimates and get closer to an accurate value for your home. But even that can only get you so far.</p>
+         <p className='leading-8 mb-10 font-[400]'>That’s where our Simple Sale™ price comes in. When you request a home value estimate, we ask our network of buyers to make you a real offer on the home. They compete for your business and you’re presented with the highest bid. Usually, this offer is around 90 - 95% of your home's market value.</p>
+         <p className='leading-8 mb-10 font-[400]'>If you want to get even closer to your home's true value, we’ll connect you with a top real estate agent near you. Local real estate agents can physically view the property, they list homes like yours every day, they know the neighborhoods, they know what's trending, and they can call out unique characteristics on the property. No online tool can do all of that.</p>
+      </section>
+   )
 }
 
 export default HomeValueEstimateInfo
