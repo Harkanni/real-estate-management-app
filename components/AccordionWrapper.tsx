0be43ff489499a886b32b922cf37f541577f7273
harkanni <harkanzyempire001@gmail.com>
2024-02-14 12:48:52 +0100

    accordion data

--- components/AccordionWrapper.tsx
+++ components/AccordionWrapper.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const AccordionWrapper = () => {
-  return (
-    <div>AccordionWrapper</div>
-  )
+   return (
+      <section className='p-16 my-5'>
+         <div>AccordionWrapper</div>
+      </section>
+   )
 }
 
 export default AccordionWrapper
--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 import { image1, image2 } from '@/assets/';
-import { FaCircleCheck } from "react-icons/fa6";
+import { FaCircleCheck } from 'react-icons/fa6';
 
 export const Evaluations = [
   {
@@ -18,52 +18,73 @@ export const Evaluations = [
 ];
 
 export const Comparisons = [
-   {
-      label: '',
-      desc: '',
-      calc: 'Online Tools',
-      agents: 'Top Agents',
-   },
-   {
-      label: 'Publicly Available Data',
-      desc: 'Compile tax records and assessments with other data sources.',
-      calc: <FaCircleCheck stroke='black' fill='#c5c8cd' size={25} color='black'/>,
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-   {
-      label: 'Recent Sales History',
-      desc: "Combine the home's last sale price with current market price.",
-      calc: <FaCircleCheck stroke='black' fill='#c5c8cd' size={25} color='black'/>,
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-   {
-      label: 'Comparable Market Analysis',
-      desc: 'Look at recent sales records for other properties in the same neighborhood.',
-      calc: <FaCircleCheck stroke='black' fill='#c5c8cd' size={25} color='black'/>,
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-   {
-      label: 'Knowledge of the Area',
-      desc: "Nearby schools, trendy restaurants, and the next door neighbor's dog all come into play.",
-      calc: '',
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-   {
-      label: 'Characteristics & Condition',
-      desc: 'A thorough property inspection is done to note any problem or home improvements that may impact the value.',
-      calc: '',
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-   {
-      label: 'Buyer Preferences',
-      desc: 'Agents regularly work with buyers and learn the current housing tastes.',
-      calc: '',
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-   {
-      label: 'Local Market Conditions',
-      desc: 'Working in the same neighborhood all year gives agents perspective on local trends.',
-      calc: '',
-      agents: <FaCircleCheck stroke='black' size={25} color='black'/>
-   },
-]
+  {
+    label: '',
+    desc: '',
+    calc: 'Online Tools',
+    agents: 'Top Agents'
+  },
+  {
+    label: 'Publicly Available Data',
+    desc: 'Compile tax records and assessments with other data sources.',
+    calc: (
+      <FaCircleCheck stroke='black' fill='#c5c8cd' size={25} color='black' />
+    ),
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  },
+  {
+    label: 'Recent Sales History',
+    desc: "Combine the home's last sale price with current market price.",
+    calc: (
+      <FaCircleCheck stroke='black' fill='#c5c8cd' size={25} color='black' />
+    ),
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  },
+  {
+    label: 'Comparable Market Analysis',
+    desc: 'Look at recent sales records for other properties in the same neighborhood.',
+    calc: (
+      <FaCircleCheck stroke='black' fill='#c5c8cd' size={25} color='black' />
+    ),
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  },
+  {
+    label: 'Knowledge of the Area',
+    desc: "Nearby schools, trendy restaurants, and the next door neighbor's dog all come into play.",
+    calc: '',
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  },
+  {
+    label: 'Characteristics & Condition',
+    desc: 'A thorough property inspection is done to note any problem or home improvements that may impact the value.',
+    calc: '',
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  },
+  {
+    label: 'Buyer Preferences',
+    desc: 'Agents regularly work with buyers and learn the current housing tastes.',
+    calc: '',
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  },
+  {
+    label: 'Local Market Conditions',
+    desc: 'Working in the same neighborhood all year gives agents perspective on local trends.',
+    calc: '',
+    agents: <FaCircleCheck stroke='black' size={25} color='black' />
+  }
+];
+
+export const AccordionData = [
+  {
+    title: 'Accordion header 1',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae ipsa officiis facilis, illum, deleniti ab quaerat id eveniet odio veritatis possimus sapiente voluptatum! Accusamus ab, omnis provident animi repellat quidem sequi adipisci at. Vel dolorem nihil amet soluta suscipit alias ex, aspernatur harum qui laudantium, itaque animi laborum aut quis.`
+  },
+  {
+    title: 'Accordion header 2',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae ipsa officiis facilis, illum, deleniti ab quaerat id eveniet odio veritatis possimus sapiente voluptatum! Accusamus ab, omnis provident animi repellat quidem sequi adipisci at. Vel dolorem nihil amet soluta suscipit alias ex, aspernatur harum qui laudantium, itaque animi laborum aut quis.`
+  },
+  {
+    title: 'Accordion header 3',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae ipsa officiis facilis, illum, deleniti ab quaerat id eveniet odio veritatis possimus sapiente voluptatum! Accusamus ab, omnis provident animi repellat quidem sequi adipisci at. Vel dolorem nihil amet soluta suscipit alias ex, aspernatur harum qui laudantium, itaque animi laborum aut quis.`
+  },
+];
