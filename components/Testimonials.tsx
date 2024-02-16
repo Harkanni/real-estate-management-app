4ba65ed745265144c6e09d35d997afcae85b0a4b
harkanni <harkanzyempire001@gmail.com>
2024-02-16 01:52:13 +0100

    testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -6,19 +6,20 @@ const Testimonials = () => {
    <section className={`p-16 my-5`}>
       <h2 className='mb-14 text-center text-4xl font-semibold'>What our clients say</h2>
       <div className={`text-center`}>
-         <span className='text-4xl font-thin text-[#18a0f8]'>4.8</span>
+         <span className='text-4xl font-thin text-[#18a0f8]' style={{ verticalAlign: 'middle'}}>4.8</span>
 
-         <span>
+         <span className={`inline-flex justify-center items-center mx-1`} >
             { 
                [1, 2, 3,4].map((item, index) => {
                   return (
-                     <span key={index}>
+                     <span key={index} className=''>
                         <MdStar color='#ffa53b' />
                      </span>
                   )
                })
             }
          </span>
+         
          <span>Google rating based on over 240+ reviews</span>
       </div>
    </section>
