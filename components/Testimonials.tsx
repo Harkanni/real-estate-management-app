b26d9dfbbf639d7d3aa38cbbbdb8ee08d60499d5
harkanni <harkanzyempire001@gmail.com>
2024-03-12 11:02:18 +0100

    testimonial

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -2,28 +2,28 @@ import React from 'react'
 import { MdStar } from "react-icons/md";
 
 const Testimonials = () => {
-  return (
-   <section className={`p-16 my-5`}>
-      <h2 className='mb-14 text-center text-4xl font-semibold'>What our clients say</h2>
-      <div className={`text-center`}>
-         <span className='text-5xl font-thin text-[#18a0f8]' style={{ verticalAlign: 'middle'}}>4.8</span>
+   return (
+      <section className={`p-16 my-5`}>
+         <h2 className='mb-14 text-center text-4xl font-semibold'>What our clients say</h2>
+         <div className={`text-center`}>
+            <span className='text-5xl font-thin text-[#18a0f8]' style={{ verticalAlign: 'middle' }}>4.8</span>
 
-         <span className={`inline-flex justify-center items-center mx-1`} >
-            { 
-               [1, 2, 3,4].map((item, index) => {
-                  return (
-                     <span key={index} className=''>
-                        <MdStar color='#ffa53b' />
-                     </span>
-                  )
-               })
-            }
-         </span>
-         
-         <span>Google rating based on over 240+ reviews</span>
-      </div>
-   </section>
-  )
+            <span className={`inline-flex justify-center items-center mx-1`} >
+               {
+                  [1, 2, 3, 4].map((item, index) => {
+                     return (
+                        <span key={index} className=''>
+                           <MdStar color='#ffa53b' />
+                        </span>
+                     )
+                  })
+               }
+            </span>
+
+            <span>Google rating based on over 240+ reviews</span>
+         </div>
+      </section>
+   )
 }
 
 export default Testimonials
